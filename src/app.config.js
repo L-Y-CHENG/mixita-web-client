cfaad31436efbe19d6347e69d41c7400d7dd6e41
liyucheng <2850188432@qq.com>
2020-07-14 08:35:06 +0800

    init

--- src/app.config.js
+++ src/app.config.js
@@ -3,9 +3,39 @@ export default {
     'pages/index/index'
   ],
   window: {
-    backgroundTextStyle: 'light',
+    navigationBarTitleText: 'M i X i T A',
+    backgroundTextStyle: 'dark',
+    backgroundColor: '#191919',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list: [{
+      pagePath: 'pages/index/index1',
+      text: 'Message',
+      iconPath: './assets/images/tab_message.png',
+      selectedIconPath: './assets/images/tab_message.png'
+    }, {
+      pagePath: 'pages/index/index2',
+      text: 'Map',
+      iconPath: './assets/images/tab_map.png',
+      selectedIconPath: './assets/images/tab_map.png'
+    }, 
+    {
+      pagePath: 'pages/index/index3',
+      text: 'Community',
+      iconPath: './assets/images/tab_community.png',
+      selectedIconPath: './assets/images/tab_community.png'
+    }, 
+    {
+      pagePath: 'pages/index/index',
+      text: 'Me',
+      iconPath: './assets/images/tab_me.png',
+      selectedIconPath: './assets/images/tab_me.png'
+    }],
+    color: '#f6f6f6',
+    selectedColor: '#CC33FF',
+    backgroundColor: '#191919',
+    borderStyle: 'white'
   }
 }
