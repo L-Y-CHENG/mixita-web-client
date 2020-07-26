fa060299c166d4a4d0f78c10062adca600c68104
L-Y-CHENG <296282000@qq.com>
2020-07-26 22:54:44 +0800

    init web

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Button, Text } from '@tarojs/components'
+import api from '../../api/api'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
@@ -23,13 +24,24 @@ import './index.scss'
 class Index extends Component {
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
+
+
   }
 
   componentWillUnmount () { }
 
-  componentDidShow () { }
+  componentDidShow () {
+    this.tryApi()
+  }
 
   componentDidHide () { }
+
+  tryApi() {
+    api.get("").then((res) => {
+      console.log('>>>>>>>>>>>>>>')
+      console.log(res)
+    })
+  }
 
   render () {
     return (
@@ -37,6 +49,7 @@ class Index extends Component {
         <Button className='add_btn' onClick={this.props.add}>+</Button>
         <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <Button className='rpc_btn' onClick={this.props.asyncAdd}>RPC-API</Button>
         <View><Text>{this.props.counter.num}</Text></View>
         <View><Text>Hello, World</Text></View>
       </View>
